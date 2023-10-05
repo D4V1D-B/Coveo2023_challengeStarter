12d334502bf11d688f8ead44b8faad9279f254f2
DABER364 <david16bernier@hotmail.ca>
2023-10-05 00:02:42 -0400

    interception
    Il arrive à intercepter la meteorite à 90%, il manque quand c'est impossible/trop proche. Il target les plus petits

--- starterkits/python/bot.py
+++ starterkits/python/bot.py
@@ -1,5 +1,4 @@
 
-
 import game_message
 from game_message import *
 from actions import *
@@ -14,35 +13,41 @@ class Bot:
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
-        #tire les smalls si reverse=True, si pas reverse tire large
-        listePriorite = sorted(game_message.meteors, key=lambda meteor: meteor.meteorType)
+        # tire les smalls si reverse=True, si pas reverse tire large
+        listePriorite = sorted(game_message.meteors, key=lambda meteor: meteor.meteorType, reverse=True)
 
 
-        a = game_message.constants.rockets.speed**2 - (game_message.constants.meteorInfos[listePriorite[0].meteorType.value].approximateSpeed**2)
-        #print(a)
-        b = 2 * (game_message.cannon.position.x * listePriorite[0].position.x + game_message.cannon.position.y * listePriorite[0].position.y)
-        #print(b)
-        c = -(((game_message.cannon.position.x - listePriorite[0].position.x)**2 + (game_message.cannon.position.y - listePriorite[0].position.y)**2)**0.5)**2
-        #print(c)
-        d = (b**2) - (4 * a * c)
-
-        sol1 = (-b-(d**0.5))/(2*a)
-        sol2 = (-b+(d**0.5)) / (2 * a)
-        if sol1 < 0 and sol2 < 0:
+        a = (game_message.constants.rockets.speed**2) - (game_message.constants.meteorInfos[listePriorite[0].meteorType.value].approximateSpeed ** 2)
+        # print(a)
+        Dx = game_message.cannon.position.x - listePriorite[0].position.x
+        Dy = game_message.cannon.position.y - listePriorite[0].position.y
+        b = 2 * (Dx * listePriorite[0].velocity.x + Dy *
+                 listePriorite[0].velocity.y)
+        # print(b)
+        c = -(((game_message.cannon.position.x - listePriorite[0].position.x) ** 2 + (
+                    game_message.cannon.position.y - listePriorite[0].position.y) ** 2) ** 0.5) ** 2
+        # print(c)
+        d = (b ** 2) - (4 * a * c)
+        if d < 0:
             return []
-        if sol1 > 0 and sol2 > 0:
-            temps = min(sol1, sol2)
         else:
-            temps = max(sol1, sol2)
+            sol1 = (-b - (d ** 0.5)) / (2 * a)
+            sol2 = (-b + (d ** 0.5)) / (2 * a)
+            if sol1 < 0 and sol2 < 0:
+                return []
+            if sol1 > 0 and sol2 > 0:
+                temps = min(sol1, sol2)
+            else:
+                temps = max(sol1, sol2)
 
-        positionx = listePriorite[0].position.x + listePriorite[0].velocity.x * temps
-        positiony = listePriorite[0].position.y + listePriorite[0].velocity.y * temps
-        #print(positionx, positiony)
-        if game_message.tick % 100 == 0:
+        positionX = listePriorite[0].position.x + listePriorite[0].velocity.x * temps
+        positionY = listePriorite[0].position.y + listePriorite[0].velocity.y * temps
+        # print(positionx, positiony)
+        if game_message.tick % 100 == 0 or game_message.tick == 999:
             print('Score ---------->', game_message.score)
         if game_message.cannon.cooldown == 0:
             return [
-                LookAtAction(Vector(x=positionx,y=positiony)),
+                LookAtAction(Vector(x=positionX, y=positionY)),
                 ShootAction(),
             ]
         else:
