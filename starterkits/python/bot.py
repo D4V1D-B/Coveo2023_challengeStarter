98ac25fab668094f90f95df964b26ce527459491
DABER364 <david16bernier@hotmail.ca>
2023-10-04 21:23:36 -0400

    tire en fonction de la taille
    titre, il faudrait maintenant implémenter en fonction de la distance et anticiper la course du projectile

--- starterkits/python/bot.py
+++ starterkits/python/bot.py
@@ -1,3 +1,4 @@
+import game_message
 from game_message import *
 from actions import *
 
@@ -11,12 +12,16 @@ class Bot:
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
-        if game_message.cannon.orientation >= 45:
-            self.direction = -1
-        elif game_message.cannon.orientation <= -45:
-            self.direction = 1
+        #tire les smalls si reverse=True
+        game_message.meteors.sort(key=lambda meteor: meteor.meteorType)
 
-        return [
-            RotateAction(angle=15 * self.direction),
-            ShootAction(),
-        ]
+        if game_message.tick % 100 == 0:
+            print('Score ---------->',game_message.score)
+        if game_message.cannon.cooldown == 0:
+            return [
+                LookAtAction(game_message.meteors[0].position),
+                ShootAction(),
+            ]
+        else:
+            return []
+
